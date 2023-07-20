65e3a0b839c49c18dc1006705de417256173026b
Mohammed Muddassir Siddiqui <1muddassir.siddiqui@gmail.com>
2023-07-20 14:44:50 +0530

    Update intune_wipe.py

--- intune_wipe.py
+++ intune_wipe.py
@@ -5,12 +5,12 @@ import os
 # Intune API endpoint for device wipe
 intune_url = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices"
 
-# Intune username and password fetched from GitHub Secrets
-intune_username = os.environ.get('INTUNE_USERNAME')
-intune_password = os.environ.get('INTUNE_PASSWORD')
+# Intune username and password fetched from GitHub Actions secrets
+intune_username = os.environ.get('INTUNE_USERNAME_SECRET')
+intune_password = os.environ.get('INTUNE_PASSWORD_SECRET')
 
-# Device ID and device name for the PC to be wipedx
-device_id = "ILDMC-BW4SQF3"
+# Device ID for the PC to be wiped
+device_id = "YOUR_DEVICE_ID"
 
 # Construct the authentication token
 token_url = "https://login.microsoftonline.com/common/oauth2/token"
@@ -35,25 +35,24 @@ headers = {
 device_search_url = f"{intune_url}/{device_id}"
 response = requests.get(device_search_url, headers=headers)
 device_details = json.loads(response.text)
-wipe_action_id = device_details['wipeActionId']
+wipe_action_id = device_details['id']
 
 # Create the request body to initiate the device wipe
 wipe_request_body = {
-    'actionReason': 'Wipe requested by user',
-    'actionType': 'wipe',
-    'deviceActionResult': {
-        '@odata.type': '#microsoft.graph.deviceActionResult',
-        'actionName': 'wipe',
-        'actionState': 'pending'
-    }
+    '@odata.type': '#microsoft.graph.deviceWipeRequest',
+    'keepUserData': False,
+    'keepEnrollmentData': False,
+    'deleteAlerts': False,
+    'deviceNotContactedSince': None,
+    'deviceState': 'wipePending'
 }
 
 # Initiate the device wipe
-wipe_action_url = f"{intune_url}/{device_id}/deviceManagement/executeAction"
+wipe_action_url = f"{intune_url}/{device_id}/wipe"
 response = requests.post(wipe_action_url, headers=headers, json=wipe_request_body)
 
 # Check the response status and display the result
-if response.status_code == 202:
+if response.status_code == 204:
     print("Device wipe initiated successfully.")
 else:
     print("Failed to initiate device wipe.")
